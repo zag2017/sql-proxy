8a1b188782e2569911f617ca8b6a2c778728fdc2
zag <zag2017@hotmail.com>
2019-12-28 22:30:21 -0500

    Add the related project directory

---
+++ learn-demo/learn_demo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// main entry
+int main(int argc,char** argv) {
+    printf("learn demo ! \n");
+    return 0;
+}
---
+++ tests/test_proxy.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// test entry
+int main(int argc,char** argv) {
+    printf("Test SQL PROXY ! \n");
+    return 0;
+}
